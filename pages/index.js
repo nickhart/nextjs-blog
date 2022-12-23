20fc4e3c1dd49e6e5419f68ba1c75b1c63d7c967
Nick Hart <nickhart@gmail.com>
2022-12-22 16:46:06 -0800

    simple change

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>I'm learning Next.js</p>
+        <p>My Boring Website</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
